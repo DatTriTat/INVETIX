eb542790820aa2bef5b266c73d8f05800ab93580
DatTriTat <dattri.tat@sjsu.edu>
2023-10-04 00:04:16 -0700

    fix login function

--- userService/src/main/java/com/example/userService/service/AuthService.java
+++ userService/src/main/java/com/example/userService/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.example.userService.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,9 +20,13 @@ public class AuthService {
     private JwtService jwtService;
 
     public String saveUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-        return "user added to the system";
+        Optional<User> credential = userRepository.findByUsernameAndPassword(user.getUserName(), user.getPassword());
+        if (credential.isEmpty()) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            userRepository.save(user);
+            return "user added to the system";
+        }
+        return "user already exist";
     }
 
     public String generateToken(String userName) {
@@ -30,6 +36,5 @@ public class AuthService {
     public void validateToken(String token) {
         jwtService.validateToken(token);
     }
-
 
 }
